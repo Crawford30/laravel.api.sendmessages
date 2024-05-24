3ae224f4c1cbc8254193026a25bd5bc9a5dfed66
Joel Crawford <joelcrawford30@gmail.com>
2024-05-24 19:51:19 +0300

    Created::Json middleware

--- app/Http/Middleware/JsonMiddleware.php
+++ app/Http/Middleware/JsonMiddleware.php
@@ -13,8 +13,9 @@ class JsonMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
+        $request->headers->set('Accept', 'application/json');
         return $next($request);
     }
 }
